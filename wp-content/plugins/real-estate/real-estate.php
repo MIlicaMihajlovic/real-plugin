38e169d0f4729949f5e58d42e0c2659931578ab1
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2019-07-19 10:25:29 +0200

    Register custom taxonomies Location and Type

--- wp-content/plugins/real-estate/real-estate.php
+++ wp-content/plugins/real-estate/real-estate.php
@@ -5,6 +5,7 @@
 
  */
 
+//Register CPT Real Estate
 function create_real_cpt() {
     $labels = array(
         'name'               => _x( 'Real Estate', 'post type general name', 'textdomain' ),
@@ -43,3 +44,69 @@ function create_real_cpt() {
 }
 
  add_action('init', 'create_real_cpt');
+
+//Register custom taxonomies Location and Type non-hierarchical
+function create_real_taxonomies() {
+
+    //Register taxonomy Location
+    $labels = array(
+        'name'                       => _x( 'Locations', 'taxonomy general name', 'textdomain' ),
+        'singular_name'              => _x( 'Location', 'taxonomy singular name', 'textdomain' ),
+        'search_items'               => __( 'Search Locations', 'textdomain' ),
+        'popular_items'              => __( 'Popular Locations', 'textdomain' ),
+        'all_items'                  => __( 'All Locations', 'textdomain' ),
+        'parent_item'                => __( 'Parent Type' ),
+        'parent_item_colon'          => __( 'Parent Type:' ),
+        'edit_item'                  => __( 'Edit Location', 'textdomain' ),
+        'update_item'                => __( 'Update Location', 'textdomain' ),
+        'add_new_item'               => __( 'Add New Location', 'textdomain' ),
+        'new_item_name'              => __( 'New Location Name', 'textdomain' ),
+        'separate_items_with_commas' => __( 'Separate locations with commas', 'textdomain' ),
+        'add_or_remove_items'        => __( 'Add or remove locations', 'textdomain' ),
+        'not_found'                  => __( 'No locations found.', 'textdomain' ),
+        'menu_name'                  => __( 'Locations', 'textdomain' ),
+    );
+
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true
+    );
+
+    register_taxonomy('location', 'real_estate', $args);
+
+    //Register taxonomy Type
+    $labels = array(
+        'name'                       => _x( 'Types', 'taxonomy general name', 'textdomain' ),
+        'singular_name'              => _x( 'Type', 'taxonomy singular name', 'textdomain' ),
+        'search_items'               => __( 'Search Types', 'textdomain' ),
+        'popular_items'              => __( 'Popular Types', 'textdomain' ),
+        'all_items'                  => __( 'All Types', 'textdomain' ),
+        'parent_item'                => __( 'Parent Type' ),
+        'parent_item_colon'          => __( 'Parent Type:' ),
+        'edit_item'                  => __( 'Edit Type', 'textdomain' ),
+        'update_item'                => __( 'Update Type', 'textdomain' ),
+        'add_new_item'               => __( 'Add New Type', 'textdomain' ),
+        'new_item_name'              => __( 'New Type Name', 'textdomain' ),
+        'separate_items_with_commas' => __( 'Separate types with commas', 'textdomain' ),
+        'add_or_remove_items'        => __( 'Add or remove types', 'textdomain' ),
+        'not_found'                  => __( 'No types found.', 'textdomain' ),
+        'menu_name'                  => __( 'Types', 'textdomain' ),
+    );
+
+    $args = array(
+        'hierarchical'          => false,
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var'             => true
+    );
+
+    register_taxonomy('type', 'real_estate', $args);
+}
+
+add_action('init', 'create_real_taxonomies');
